e9819b3271854b42d3edc63276ce379468ff1800
Nolan Chen <43057150+nolanlc@users.noreply.github.com>
2023-11-26 22:41:39 -0800

    Update hello_chat.py

--- hello_chat.py
+++ hello_chat.py
@@ -1,29 +1,38 @@
 import boto3
 
 from langchain.chat_models import BedrockChat
-from langchain.schema import HumanMessage
 
 from langchain.chains import LLMChain
-from langchain.prompts import HumanMessagePromptTemplate, ChatPromptTemplate
+from langchain.prompts import MessagesPlaceholder, HumanMessagePromptTemplate, ChatPromptTemplate
+from langchain.memory import ConversationBufferMemory
 
 modelId = 'anthropic.claude-v2'
 
 chat = BedrockChat(model_id=modelId, region_name = 'us-west-2', model_kwargs={"temperature": 0.1})
 
+memory = ConversationBufferMemory(memory_key="messages", return_messages=True)
+
 prompt = ChatPromptTemplate(
-    input_variables=["content"],
+    input_variables=["content", "messages"],
     messages=[
+        MessagesPlaceholder(variable_name="messages"),
         HumanMessagePromptTemplate.from_template("{content}")
         ]
 )
 
 chain = LLMChain(
     llm=chat,
-    prompt=prompt
+    prompt=prompt,
+    memory=memory
 )
 
 while True:
     content = input(">> ")
+    
     result = chain({"content": content})
     
-    #print (result["text"])
+    print (result["text"])
+    
+    
+    
+    
