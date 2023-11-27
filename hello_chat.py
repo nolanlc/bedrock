a68b9ee3ff5405464322910bf91a13d14955623e
Nolan Chen <43057150+nolanlc@users.noreply.github.com>
2023-11-26 22:06:55 -0800

    Update hello_chat.py

--- hello_chat.py
+++ hello_chat.py
@@ -9,7 +9,6 @@ from langchain.prompts import HumanMessagePromptTemplate, ChatPromptTemplate
 modelId = 'anthropic.claude-v2'
 
 chat = BedrockChat(model_id=modelId, region_name = 'us-west-2', model_kwargs={"temperature": 0.1})
-#chat = BedrockChat(model_id=modelId, model_kwargs={"temperature": 0.1})
 
 prompt = ChatPromptTemplate(
     input_variables=["content"],
